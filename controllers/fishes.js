37d706aacf204779a3627bf0d6db13d77cf9f38c
JeevanBodigam <bodigamjeevan296@gmail.com>
2021-04-11 20:00:37 -0500

    added create,update,detail views

--- controllers/fishes.js
+++ controllers/fishes.js
@@ -12,9 +12,17 @@ exports.fish_list = async function (req, res) {
     // res.send('NOT IMPLEMENTED: Fish list');
 };
 // for a specific Fish.
-exports.fish_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Fish detail: ' + req.params.id);
+exports.fish_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await Fish.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
+
 // Handle Fish create on POST.
 exports.fish_create_post = async function (req, res) {
     console.log(req.body)
@@ -22,7 +30,7 @@ exports.fish_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costumetype":"goat", "cost":12, "size":"large"}
+    // {"Fishtype":"goat", "cost":12, "size":"large"}
     document.fishname = req.body.fishname;
     document.habitat = req.body.habitat;
     document.classification = req.body.classification;
@@ -36,14 +44,38 @@ exports.fish_create_post = async function (req, res) {
         res.status(500);
     }
 };
-// Handle Fish delete form on DELETE.
-exports.fish_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Fish delete DELETE ' + req.params.id);
+// Handle Fish delete on DELETE.
+exports.fish_delete = async function(req, res) {
+    console.log("delete "  + req.params.id)
+    try {
+        result = await Fish.findByIdAndDelete( req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
+
 // Handle Fish update form on PUT.
-exports.fish_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Fish update PUT' + req.params.id);
+exports.fish_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Fish.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.fishname) toUpdate.fishname = req.body.fishname;
+        if(req.body.habitat) toUpdate.habitat = req.body.habitat;
+        if(req.body.classification) toUpdate.classification = req.body.classification;
+        if(req.body.price) toUpdate.price=req.body.price
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
 
 // VIEWS
 // Handle a show all view
@@ -58,3 +90,61 @@ exports.fish_view_all_Page = async function (req, res) {
         res.status(500);
     }
 };
+
+// Handle a show one view with id specified by query
+exports.fish_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await Fish.findById( req.query.id)
+        res.render('fishdetail', 
+{ title: 'Fish Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
+// Handle building the view for creating a fish.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.fish_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('fishcreate', { title: 'Fish Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a fish.
+// query provides the id
+exports.fish_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await Fish.findById(req.query.id)
+        res.render('fishupdate', { title: 'Fish Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.fish_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await Fish.findById(req.query.id)
+        res.render('fishdelete', { title: 'Fish Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
--- routes/fishes.js
+++ routes/fishes.js
@@ -6,3 +6,19 @@ const fish_controlers= require('../controllers/fishes');
 router.get('/', fish_controlers.fish_view_all_Page );
 module.exports = router;
 
+/* GET detail fish page */
+router.get('/detail', fish_controlers.fish_view_one_Page);
+
+/* GET create fish page */
+router.get('/create', fish_controlers.fish_create_Page);
+
+/* GET create update page */
+router.get('/update', fish_controlers.fish_update_Page);
+
+/* GET create fish page */
+router.get('/delete', fish_controlers.fish_delete_Page);
+
+
+
+
+

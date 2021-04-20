40d2049145a562917a3a9f6e263d9920d019b050
JeevanBodigam <bodigamjeevan296@gmail.com>
2021-04-19 20:36:17 -0500

    modified few chnages

--- app.js
+++ app.js
@@ -52,26 +52,28 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-  Account.findOne({ username: username }, function (err, user) {
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
-  });
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
   }));
-  app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -84,7 +86,7 @@ app.use('/resource', resourceRouter)
 // passport config
 // Use the existing connection
 // The Account model
-var Account =require('./models/account');
+var Account = require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
--- routes/fishes.js
+++ routes/fishes.js
@@ -4,7 +4,7 @@ const fish_controlers= require('../controllers/fishes');
 
 /* GET costumes */
 router.get('/', fish_controlers.fish_view_all_Page );
-module.exports = router;
+
 
 /* GET detail fish page */
 router.get('/detail', fish_controlers.fish_view_one_Page);
@@ -17,6 +17,8 @@ router.get('/update', fish_controlers.fish_update_Page);
 
 /* GET create fish page */
 router.get('/delete', fish_controlers.fish_delete_Page);
+
+module.exports = router;
 
 
 
